e57c262c860414b4854c79c442b63abdf1548850
Ghofranbtk <ghofran.bentk@gmail.com>
2023-12-01 16:00:12 +0100

    add interfaces

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 
-diseases_list = []
-diseases_symptoms = []
+prophets_list = []
+prophets_symptoms = []
 symptom_map = {}
 d_desc_map = {}
 d_treatment_map = {}
 
 #loads the knowledge from .txt files into variables to allow the code to use it
 def preprocess():
-    #global diseases_list, diseases_symptoms, symptom_map, d_desc_map, d_treatment_map
-    diseases = open("diseases.txt")
-    diseases_t = diseases.read()
-    diseases_list = diseases_t.split("\n")
-    diseases.close()
+    #global prophets_list, prophets_symptoms, symptom_map, d_desc_map, d_treatment_map
+    prophets = open("prophets.txt")
+    prophets_t = prophets.read()
+    prophets_list = prophets_t.split("\n")
+    prophets.close()
 
-    for disease in diseases_list:
-        disease_s_file = open("Disease symptoms/" + disease + ".txt")
-        disease_s_data = disease_s_file.read()
-        s_list = disease_s_data.split("\n")
-        diseases_symptoms.append(s_list)
-        symptom_map[str(s_list)] = disease
-        disease_s_file.close()
+    for prophet in prophets_list:
+        prophet_s_file = open("Prophet symptoms/" + prophet + ".txt")
+        prophet_s_data = prophet_s_file.read()
+        s_list = prophet_s_data.split("\n")
+        prophets_symptoms.append(s_list)
+        symptom_map[str(s_list)] = prophet
+        prophet_s_file.close()
 
-        disease_s_file = open("Disease descriptions/" + disease + ".txt")
-        disease_s_data = disease_s_file.read()
-        d_desc_map[disease] = disease_s_data
-        disease_s_file.close()
+        prophet_s_file = open("Prophet descriptions/" + prophet + ".txt")
+        prophet_s_data = prophet_s_file.read()
+        d_desc_map[prophet] = prophet_s_data
+        prophet_s_file.close()
 
-        disease_s_file = open("Disease treatments/" + disease + ".txt")
-        disease_s_data = disease_s_file.read()
-        d_treatment_map[disease] = disease_s_data
-        disease_s_file.close()
+        prophet_s_file = open("Prophet treatments/" + prophet + ".txt")
+        prophet_s_data = prophet_s_file.read()
+        d_treatment_map[prophet] = prophet_s_data
+        prophet_s_file.close()
 
 
-def identify_disease(*arguments):
+def identify_prophet(*arguments):
     symptom_list = []
     for symptom in arguments:
         symptom_list.append(symptom)
@@ -40,23 +40,23 @@ def identify_disease(*arguments):
     return symptom_map[str(symptom_list)]
 
 
-def get_details(disease):
-    return d_desc_map[disease]
+def get_details(prophet):
+    return d_desc_map[prophet]
 
 
-def get_treatments(disease):
-    return d_treatment_map[disease]
+def get_treatments(prophet):
+    return d_treatment_map[prophet]
 
 
-def if_not_matched(disease):
+def if_not_matched(prophet):
     print("")
-    id_disease = disease
-    disease_details = get_details(id_disease)
-    treatments = get_treatments(id_disease)
+    id_prophet = prophet
+    prophet_details = get_details(id_prophet)
+    treatments = get_treatments(id_prophet)
     print("")
-    print("The most probable disease that you have is %s\n" % (id_disease))
-    print("A short description of the disease is given below :\n")
-    print(disease_details + "\n")
+    print("The most probable prophet that you have is %s\n" % (id_prophet))
+    print("A short description of the prophet is given below :\n")
+    print(prophet_details + "\n")
     print(
         "The common medications and procedures suggested by other real doctors are: \n"
     )
